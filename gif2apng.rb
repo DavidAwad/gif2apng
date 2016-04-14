fc45bfa8f617bcb7a6afd0f77d0d3206672862a3
David Awad <Davidawad64@gmail.com>
2016-04-13 20:09:21 -0400

    Update gif2apng.rb

--- gif2apng.rb
+++ gif2apng.rb
@@ -3,7 +3,7 @@ class Gif2apng < Formula
   homepage "http://github.com/davidawad/"
   url "https://github.com/davidawad/gif2apng/archive/1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "12fb705e32e0cc8d6415be9fc8355be8d2b0fe5286d40094d4da37ce0652e1de"
+  # sha256 "12fb705e32e0cc8d6415be9fc8355be8d2b0fe5286d40094d4da37ce0652e1de"
 
   def install
       puts "thanks for installing! gif2apng usage is straightforward."
