4fde996c6bf3080e2480bee4572bb484dfd12f06
David Awad <davidawad64@gmail.com>
2016-04-15 14:09:19 -0400

    dist attempts

--- gif2apng.rb
+++ gif2apng.rb
@@ -1,14 +1,14 @@
 class Gif2apng < Formula
   desc ""
-  homepage "http://github.com/davidawad/"
-  url "https://github.com/davidawad/gif2apng/archive/1.0.0.tar.gz"
+  homepage "http://github.com/davidawad/homebrew-gif2apng"
+  url "https://github.com/davidawad/homebrew-gif2apng/archive/1.0.0.tar.gz"
   version "1.0.0"
-  # sha256 "12fb705e32e0cc8d6415be9fc8355be8d2b0fe5286d40094d4da37ce0652e1de"
+  sha256 "12fb705e32e0cc8d6415be9fc8355be8d2b0fe5286d40094d4da37ce0652e1de"
 
   def install
       puts "Thanks for installing! Swag and Marriment await - David Awad."
       # chmod u+x gif2apng
-      # sudo mv gif2apng /usr/local/bin
+      sudo mv gif2apng /usr/local/bin
       #
         # ENV.deparallelize  # if your formula fails when building in parallel
 
